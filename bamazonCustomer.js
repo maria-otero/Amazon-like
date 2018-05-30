e6511dee65d2400a128a604ec1fb22cdc19439de
M Jesús Otero <mjesuotero@airbears2-10-142-167-33.airbears2.1918.berkeley.edu>
2018-05-30 10:11:34 -0700

    renaming functions and debuggin

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,61 +21,8 @@ connection.connect(function(err) {
     showAllProducts();
 });  
 
-// Promp the user what product he want, and how many of them
-function productInfo() {
-    inquirer.prompt([
-        {
-            name: "product",
-            type: "input",
-            message: "What is de Item ID of the product you would like to buy?",
-        }, {
-            name: "quantity",
-            type: "input",
-            message: "How many units of this product would you like to buy?",
-        }
-    ]).then(function(res) {
-        var idChosen = parseInt(res.product);
-        var quantityChosen =  parseInt(res.quantity);
-        
 
-        connection.query("SELECT * FROM products WHERE id = " + idChosen,
-            function(err, response) {
-                if (err) throw err;
 
-                if (response.length === 0) {
-                    console.log("\nERROR: Select a valid Item ID from Products list.");
-                    showAllProducts();
-                } else {
-                    // Response if the quentity resquested by the user is in stock
-                    var productRes = response[0];
-                    if (quantityChosen <= productRes.stock_quantity) {
-                        console.log("Your product is in stock... placing your order!");
-
-                        // Updating inventory
-                        var updateInventory = "UPDATE products SET stock_quantity = " + (productRes.stock_quantity - quantityChosen) + "WHERE id = " + idChosen;
-
-                        connection.query(updateInventory, function(err, data) {
-                            if (err) throw err;
-
-                            console.log("Your ordeer has been placed! Your total is" + productRes.price * quantityChosen);
-                            console.log("Thank you for shopping with us!");
-                            console.log("-----------------------------------------------------\n");
-                            keepShopping();
-                        })
-                    } else {
-                        console.log("Sorry, item's not in stock to place your order.\n" + 
-                            "Please change your order.\n" + 
-                            "Your item was " + productRes.product_name + " left in stock.");
-                        keepShopping();
-
-                    }
-                }
-            })
-        })
-}
-
-    
-    
 // Show all products into a table
 function showAllProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
@@ -85,9 +32,46 @@ function showAllProducts() {
             console.log(" Item ID: " + res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + "$ " + res[i].price.toString() + " | " + res[i].stock_quantity.toString() + " UNITS");
         }
         console.log("------------------------------------------------------\n");
-        productInfo();
+        buyItem();
     });
 }
+
+
+
+// Promp the user what product he want, and how many of them
+function buyItem() {
+    inquirer.prompt([
+        {
+            name: "chosenID",
+            type: "input",
+            message: "What is de Item ID of the product you would like to buy?",
+            filter: Number
+        }, {
+            name: "chosenQuantity",
+            type: "input",
+            message: "How many units of this product would you like to buy?",
+            filter: Number
+        } 
+    ]).then(function(answer) {
+        // Updating inventory
+        var updateInventory = "UPDATE products SET stock_quantity = " + (stock_quantity - answer.chosenQuantity) + "WHERE id = " + answer.chosenID;
+
+        connection.query(updateInventory, function(err, data) {
+            if (err) throw err;
+
+            // Show the price of the product... how can I grab de price of the chocen ID from the table??????
+            console.log("Your ordeer has been placed! Your total is" + productID.price * answer.chosenQuantity);
+
+            console.log("Thank you for shopping with us!");
+            console.log("-----------------------------------------------------\n");
+
+            keepShopping();
+
+        }); 
+    })
+}
+
+
 
 
 // Promp the user if he whan to keep shopping
@@ -99,8 +83,8 @@ function keepShopping() {
             type: "confirm",
             message: "Would you like to keep shopping?"
         }
-    ]).then(function(res) {
-        if (res.confirm) {
+    ]).then(function(ranswer) {
+        if (answer.confirm) {
             console.log("-----------------------------------------------------");
             showAllProducts();
         } else {
@@ -109,3 +93,24 @@ function keepShopping() {
         }
     })
 }
+
+
+
+// connection.query("SELECT * FROM products WHERE ?", 
+// { id: answer.chosenID}, function(err, res) {
+//     if (err) throw err;
+    
+//     // Response if product exist
+//     if (answer.chosenID === productID) {
+//         console.log("We are proccesing your order!");
+//     } else {
+//         console.log("Plsease choce an ID that exist.");
+//     }
+//     // Response if there are enough stock
+//     if (answer.chosenQuantity <= productStock) {
+//         console.log("Your product is in stock... placing your order!");
+//     } else {
+//         console.log("Sorry, item's not in stock to place your order.\n" + 
+//         "Please change your order.\n");
+//         keepShopping();
+//     }
